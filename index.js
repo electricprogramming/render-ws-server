94ec9d4f3e0753f500e90c50f87a64c8021a2898
electricprogramming <yeungj316@gmail.com>
2025-07-14 16:17:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Allow CORS from any origin
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js on Render!');
+  res.send('CORS-enabled for all origins!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
