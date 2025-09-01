5e357dda9f44ebaa1b5b0d0b1930ba2b1c242ae6
electricprogramming <yeungj316@gmail.com>
2025-09-01 16:59:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ wss.on('connection', (ws) => {
     // Send to all other connected clients
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(`📢 Broadcast: ${message}`);
+        client.send(message);
       }
     });
   });
